94335a9dd44fa70f3c159e2b892def1cbe40a5e6
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-07-10 15:26:59 +0300

    added details page for product

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,18 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+
+        res.render('shop/product-details', {
+            product: product,
+            pageTitle: product.title,
+            path: '/products'
+        });
+    });
+}
+
 
 exports.getCart = (req, res, next) => {
     res.render('shop/cart', {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getIndex, getCart, getCheckout, getOrders } = require('../controllers/shop');
+const { getProducts, getIndex, getCart, getCheckout, getOrders, getProduct } = require('../controllers/shop');
 
 router.get('/', getIndex);
 
 router.get('/products', getProducts);
+router.get('/products/:productId', getProduct);
 router.get('/cart', getCart);
 router.get('/checkout', getCheckout);
 router.get('/orders', getOrders);
